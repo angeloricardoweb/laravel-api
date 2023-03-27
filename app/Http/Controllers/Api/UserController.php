e4dee9d0fc1a1bf3a308465cf8f6e643b1200af9
Angelo Ricardo <angeloengcomp@gmail.com>
2023-03-26 23:57:44 -0300

    feat(users): add user update and destroy

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreUpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -40,5 +41,27 @@ class UserController extends Controller
         $user = User::findOrFail($id);
 
         return new UserResource($user);
+    }
+
+    public function update(StoreUpdateUserRequest $request, string $id)
+    {
+        $user = User::findOrFail($id);
+
+        $data = $request->validated();
+
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        return new UserResource($user);
+    }
+
+    public function destroy(string $id)
+    {
+        User::findOrFail($id)->delete();
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,5 @@ Route::get('/', function () {
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::patch('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
